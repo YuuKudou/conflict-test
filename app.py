5a6e2ba139166f933cdf27c2bae32fa718423f58
Kyosuke Sakiyama <k.sakiyama.sys21@morijyobi.ac.jp>
2022-03-22 10:37:02 +0900

    円の面積計算機能追加

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 
-app = Flask(_name_)
 
-@app.route("/")
-def top_page():
-    return render_template("index.html")
+@app.route("/circle_input")
+def circle_input():
+    return render_template("circle_input.html")
 
-if_name_ == "_name_":
-    app.run(debug=True)
+@app.route("/circle_result")
+def circle_result():
+    radius = int(request.args.get("radius"))
+    result = 3.14 * radius ** 2
+    return render_template("circle_resurt.html", result=result)
